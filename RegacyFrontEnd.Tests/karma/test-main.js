3bc8e4ba516d9f126d5e3aac16330bd8cb2359cd
haruanri <nanteneus@hotmail.co.jp>
2019-09-24 21:42:21 +0900

    不要なconsole.logを消した

--- RegacyFrontEnd.Tests/karma/test-main.js
+++ RegacyFrontEnd.Tests/karma/test-main.js
@@ -1,6 +1,5 @@
 ﻿const tests = [];
 for (var file in window.__karma__.files) {
-    console.log(file);
     if (window.__karma__.files.hasOwnProperty(file)) {
         if (/Spec\.js$/.test(file)) {
             tests.push(file);
